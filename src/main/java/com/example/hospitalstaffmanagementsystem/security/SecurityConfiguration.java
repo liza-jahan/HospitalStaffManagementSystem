e1800e0c47a3dca74dd7e49dcc17f50464415911
Eakteker jahan <eaktekerjahan@gmail.com>
2023-05-27 16:46:00 +0600

    spring Boot project

--- src/main/java/com/example/hospitalstaffmanagementsystem/security/SecurityConfiguration.java
+++ src/main/java/com/example/hospitalstaffmanagementsystem/security/SecurityConfiguration.java
@@ -1,20 +1,22 @@
 package com.example.hospitalstaffmanagementsystem.security;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
+@Configuration
 public class SecurityConfiguration {
 
 
     @Bean
-    public InMemoryUserDetailsManager createUserDetailsManager(){
-       UserDetails userDetails= User.withDefaultPasswordEncoder()
+    public InMemoryUserDetailsManager createUserDetailsManager() {
+        UserDetails userDetails = User.withDefaultPasswordEncoder()
                 .username("Liza")
                 .password("liza123")
-               .roles("USER","ADMIN")
-               .build();
+                .roles("USER", "ADMIN")
+                .build();
         return new InMemoryUserDetailsManager(userDetails);
     }
 }
