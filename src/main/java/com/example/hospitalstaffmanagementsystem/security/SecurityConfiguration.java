b9de4bfc7a08d8cd71062252ed4295cb874e8be4
Eakteker jahan <eaktekerjahan@gmail.com>
2023-05-29 02:16:33 +0600

    spring Boot project

--- src/main/java/com/example/hospitalstaffmanagementsystem/security/SecurityConfiguration.java
+++ src/main/java/com/example/hospitalstaffmanagementsystem/security/SecurityConfiguration.java
@@ -18,18 +18,25 @@ public class SecurityConfiguration {
     @Bean
     public InMemoryUserDetailsManager createUserDetailsManager() {
 
-        Function<String,String> passwordEncoder= input -> passwordEncoder().encode(input);
-        UserDetails userDetails = User.builder()
-                .passwordEncoder(passwordEncoder)
-                .username("Liza")
-                .password("liza123")
-                .roles("USER", "ADMIN")
-                .build();
+        UserDetails userDetails = createNewUser("Liza", "liza123");
         return new InMemoryUserDetailsManager(userDetails);
     }
+
+     private  UserDetails createNewUser(String username,String password)
+     {
+        Function<String, String> passwordEncoder = input -> passwordEncoder().encode(input);
+        UserDetails userDetails = User.builder()
+                .passwordEncoder(passwordEncoder)
+                .username(username)
+                .password(password)
+                .roles("USER", "ADMIN")
+                .build();
+        return userDetails;
+    }
+
     @Bean
-    public PasswordEncoder passwordEncoder(){
-        return  new BCryptPasswordEncoder();
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 
 }
