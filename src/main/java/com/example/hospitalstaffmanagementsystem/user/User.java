427808c8c41800b74cd3cb0b7864835245a5788d
Eakteker jahan <eaktekerjahan@gmail.com>
2023-06-10 04:54:49 +0600

    spring Boot project

--- src/main/java/com/example/hospitalstaffmanagementsystem/user/User.java
+++ src/main/java/com/example/hospitalstaffmanagementsystem/user/User.java
@@ -1,4 +1,10 @@
 package com.example.hospitalstaffmanagementsystem.user;
 
 public class User {
+    private  String email;
+    private  String name;
+    private  String password;
+    private  String registrationNumber;
+
+
 }
