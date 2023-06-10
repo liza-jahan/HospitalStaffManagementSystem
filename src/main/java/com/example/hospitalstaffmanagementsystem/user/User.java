c865fa49ede27febce128a3455a569b345dd9325
Eakteker jahan <eaktekerjahan@gmail.com>
2023-06-11 01:52:40 +0600

    spring Boot project

--- src/main/java/com/example/hospitalstaffmanagementsystem/user/User.java
+++ src/main/java/com/example/hospitalstaffmanagementsystem/user/User.java
@@ -1,10 +1,76 @@
 package com.example.hospitalstaffmanagementsystem.user;
 
+import nonapi.io.github.classgraph.json.Id;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+
 public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private  int id;
     private  String email;
     private  String name;
     private  String password;
     private  String registrationNumber;
 
+    public User(int id, String email, String name, String password, String registrationNumber) {
+        this.id = id;
+        this.email = email;
+        this.name = name;
+        this.password = password;
+        this.registrationNumber = registrationNumber;
+    }
 
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getRegistrationNumber() {
+        return registrationNumber;
+    }
+
+    public void setRegistrationNumber(String registrationNumber) {
+        this.registrationNumber = registrationNumber;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", email='" + email + '\'' +
+                ", name='" + name + '\'' +
+                ", password='" + password + '\'' +
+                ", registrationNumber='" + registrationNumber + '\'' +
+                '}';
+    }
 }
